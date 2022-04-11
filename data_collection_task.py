f193c1bb58bf76c52cae2a0955bf6da8f8a034cf
Clyde Ivan Fronda <78827014+TheRandomizer66@users.noreply.github.com>
2022-04-12 10:51:25 +1200

    Adding a foundation structure
    This is just a template for the program.

---
+++ data_collection_task.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+class Interface:
+    def __init__(self):
+        pass
+
+
+class Data:
+    def __init__(self):
+        pass
+
+
+root = Tk()
+gui = Interface
+data = Data
+root.mainloop()
